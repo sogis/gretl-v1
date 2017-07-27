638a7a83e6c646e2a3b4a9218b3b63f2038dfa07
bjsvwjek <jekero@outlook.com>
2017-07-27 16:29:39 +0200

    Added jdbc DriverManager.registerDriver() calls to fix not found drivers when deployed / Switched to maven repos for sqlite, postgres

--- src/main/java/ch/so/agi/gretl/util/DbConnector.java
+++ src/main/java/ch/so/agi/gretl/util/DbConnector.java
@@ -4,14 +4,25 @@ import ch.so.agi.gretl.logging.GretlLogger;
 import ch.so.agi.gretl.logging.LogEnvironment;
 
 import java.sql.Connection;
+import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.HashMap;
 
 /** Utility-Class to open connections **/
 public class DbConnector {
+
+    private static HashMap<String, String> jdbcDriverClasses = null;
+
     private static Connection con=null;
     private static GretlLogger log = LogEnvironment.getLogger(DbConnector.class) ;
 
+    static{
+        jdbcDriverClasses = new HashMap<String, String>();
+        jdbcDriverClasses.put("postgresql","org.postgresql.Driver");
+        jdbcDriverClasses.put("sqlite","org.sqlite.JDBC");
+        jdbcDriverClasses.put("derby","org.apache.derby.jdbc.EmbeddedDriver");
+    }
 
     /**
      * Returns the connection to a specific database. The database is specified by the arguments ConnectionUrl,
@@ -24,6 +35,29 @@ public class DbConnector {
      */
     public static Connection connect(String ConnectionUrl, String UserName, String Password) {
         try {
+
+            String[] splits = ConnectionUrl.split(":");
+            if (splits.length < 3)
+                throw new IllegalArgumentException("Connection string is malformed: " + ConnectionUrl);
+
+            String driverType = splits[1];
+            //hash
+            String driverClassName = jdbcDriverClasses.get(driverType);
+            if(driverClassName == null)
+                throw new IllegalArgumentException(
+                        "Configuration error. ConnectionUrl contains unsupported driver type: " + driverType + "(" + ConnectionUrl + ")");
+
+
+            Driver driver = null;
+
+            try {
+                driver = (Driver)Class.forName(driverClassName).newInstance();
+            }
+            catch(Exception e){
+                throw new RuntimeException("Could not find and load jdbc Driver Class " + driverClassName, e);
+            }
+
+            DriverManager.registerDriver(driver);
 
             con = DriverManager.getConnection(
                     ConnectionUrl,UserName,Password);
@@ -43,7 +77,8 @@ public class DbConnector {
                     log.info(f.toString());
                 }
             }
-            throw new RuntimeException("Could not connect with database: ", e);
+            log.error("Could not connect to: " + ConnectionUrl, e);
+            throw new RuntimeException("Could not connect to: " + ConnectionUrl, e);
         }
         return con;
     }
