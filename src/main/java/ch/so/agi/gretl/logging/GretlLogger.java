2c9bb7ee27dc0024b18bd64e8a162c3ab3d65b93
barpastu <noemi.sturm@bd.so.ch>
2017-08-03 07:14:18 +0200

    Refactoring livecycle to lifecycle

--- src/main/java/ch/so/agi/gretl/logging/GretlLogger.java
+++ src/main/java/ch/so/agi/gretl/logging/GretlLogger.java
@@ -2,16 +2,16 @@ package ch.so.agi.gretl.logging;
 
 /**
  * Logger used in all GRETL classes. As Gretl is based on a build tool, it
- * uses the special livecycle log level which informs gretl users on start and
+ * uses the special lifecycle log level which informs gretl users on start and
  * finish of the processing of one step.
- * Consequently, each step must have exactly two livecycle log outputs.
+ * Consequently, each step must have exactly two lifecycle log outputs.
  * One when starting the execution of the Step (after inputvalidation).
  * One after finishing the execution of the Step (after cleanup).
  * Info is used for more detailed feedback to the user.
  * Debug is used for very detailed output that should help in debugging a problem.
  * Error is used to log Exception messages.
- * Priority of the logOutput: error > livecycle > info > debug.
- * Example: Setting the loglevel to livecycle means that livecycle and error logs will be output
+ * Priority of the logOutput: error > lifecycle > info > debug.
+ * Example: Setting the loglevel to lifecycle means that lifecycle and error logs will be output
  */
 public interface GretlLogger {
 
@@ -21,5 +21,5 @@ public interface GretlLogger {
 
     public void error(String msg, Throwable thrown);
 
-    public void livecycle(String msg);
+    public void lifecycle(String msg);
 }
