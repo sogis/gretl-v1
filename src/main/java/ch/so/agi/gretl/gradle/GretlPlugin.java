c16ecc6b8f251d122e143ae7d013855dcab8866a
claeis <claeis@users.noreply.github.com>
2017-09-16 15:00:41 +0200

    make gretl a gradle plugin

---
+++ src/main/java/ch/so/agi/gretl/gradle/GretlPlugin.java
@@ -0,0 +1,14 @@
+package ch.so.agi.gretl.gradle;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+public class GretlPlugin implements Plugin<Project> {
+
+	@Override
+	public void apply(Project project) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
