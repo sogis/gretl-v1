c43f4fb502e094c3d901d288d42972750d87b82c
Christoph Raaflaub <raaflaub@puzzle.ch>
2017-11-09 10:24:45 +0100

    Jenkins API test of dockered Jenkins

---
+++ docker/start-jenkins.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "=============================================================="
+echo "Docker container for test jenkins"
+echo "Uses the following container definition:"
+echo "https://github.com/openshift/jenkins"
+echo "=============================================================="
+
+# set permission for jenkins data directory
+#chmod 777 `pwd`/jenkins/data
+
+#docker run -it \
+#	--name jenkins_my \
+#	-e JENKINS_PASSWORD=admin1234 \
+#	-p 8080:8080 \
+#	-v `pwd`/jenkins/data:/var/lib/jenkins \
+#	openshift/jenkins-2-centos7
+
+
+docker run -it \
+	--name jenkins_my \
+	-e JENKINS_PASSWORD=admin1234 \
+	-p 8080:8080 \
+	openshift/jenkins-2-centos7
