dd73e4a55ee5dcbfd2c0aca6294f2581b038a7a1
Christoph Raaflaub <ch.raaflaub@gmail.com>
2017-12-11 16:41:37 +0100

    Update gretl-runtime-build.groovy
    wait on build

--- openshift/pipeline/gretl-runtime-build.groovy
+++ openshift/pipeline/gretl-runtime-build.groovy
@@ -38,27 +38,39 @@ pipeline {
                     timeout(20) {
                     	def ocDir = tool "oc3.7.0"
                         withEnv(["PATH+OC=${ocDir}"]) {
-                    		sh "oc version"
-    		                openshift.withCluster("${params.openShiftCluster}", "${params.openShiftProject}_deploy_token") {
-        					    openshift.withProject("${params.openShiftProject}") {
-        					        echo "Running in project: ${openshift.project()}"
-        					        def buildSelector = openshift.startBuild("gretl","--from-dir=./build-tmp/")
-        							def result = buildSelector.logs('-f')
+                            sh "oc version"
+                            openshift.withCluster("${params.openShiftCluster}", "${params.openShiftProject}_deploy_token") {
+				openshift.verbose()
+        			openshift.withProject("${params.openShiftProject}") {
+        			    echo "Running in project: ${openshift.project()}"
+
+        			    def builds = openshift.startBuild("gretl","--from-dir=./build-tmp/")
+                                    builds.untilEach(1) {
+                                        echo "Created builds so far: ${it.names()}"
+
+                                        return it.object().status.phase == "Complete"
+                                    }
+        			}
+				    
+				    
+				    
+                                  //  echo "Running in project: ${openshift.project()}"
+                                  //  def builds = openshift.startBuild("gretl","--from-dir=./build-tmp/")
+					
+					
+                                 //  def result = buildSelector.logs('-f')
         							
-        						    // You can even see exactly what oc command was executed.
+                                    // You can even see exactly what oc command was executed.
                                     //echo "Logs executed: ${result.actions[0].cmd}"
                                     
                                     // And even obtain the standard output and standard error of the command.
-                                    def logsString = result.actions[0].out
-                                    def logsErr = result.actions[0].err
+                                 //   def logsString = result.actions[0].out
+                                 //   def logsErr = result.actions[0].err
         							//println logsErr
         							
-                                    echo "Build status: ${result.out}"
-        					    }
-    						}
-    					}
+                                //    echo "Build status: ${result.out}"
                     }
-				}
+		}
             }
         }
     }
