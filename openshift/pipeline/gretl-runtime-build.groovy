529e394011e6450aa3cc2d8b696d8cd37502f1df
Christoph Raaflaub <ch.raaflaub@gmail.com>
2017-12-11 15:39:46 +0100

    Update gretl-runtime-build.groovy
    Build logic with parameter

--- openshift/pipeline/gretl-runtime-build.groovy
+++ openshift/pipeline/gretl-runtime-build.groovy
@@ -3,13 +3,74 @@ pipeline {
 
     parameters {
         string(defaultValue: "TEST", description: 'What environment?', name: 'userFlag')
+        string(name: 'buildProject',
+               description: 'Which project builds the GRETL jar?',
+               defaultValue: 'sogis/gretl-multibranch/master')
+        string(name: 'openShiftCluster',
+               description: 'Which OpenShift Cluster should be used?',
+               defaultValue: 'OpenShiftAioProduction')
+        string(name: 'openShiftProject',
+               description: 'Which OpenShift project should be used?',
+               defaultValue: 'sogis-gretl-dev')
     }
 
     stages {
-        stage("foo") {
+        stage('prepare') {
             steps {
-                echo "flag: ${params.userFlag}"
+                    sh 'rm -rf build-tmp'
+                    sh 'mkdir build-tmp'
+                    
+                    sh 'cp -R docker/gretl/* build-tmp'
+                    
+                    dir('build-tmp') {
+                        step([$class     : 'CopyArtifact',
+                              projectName: "${params.buildProject}",
+                              flatten    : true
+                        ]);
+                    }
+                    
+                    sh 'ls -la build-tmp'
             }
+        }
+        stage('deploy') {
+            steps {
+                script{
+                    timeout(20) {
+                    	def ocDir = tool "oc3.7.0"
+                        withEnv(["PATH+OC=${ocDir}"]) {
+                    		sh "oc version"
+    		                openshift.withCluster("${params.openShiftCluster}", "${params.openShiftProject}_deploy_token") {
+        					    openshift.withProject("${params.openShiftProject}") {
+        					        echo "Running in project: ${openshift.project()}"
+        					        def buildSelector = openshift.startBuild("gretl","--from-dir=./build-tmp/")
+        							def result = buildSelector.logs('-f')
+        							
+        						    // You can even see exactly what oc command was executed.
+                                    //echo "Logs executed: ${result.actions[0].cmd}"
+                                    
+                                    // And even obtain the standard output and standard error of the command.
+                                    def logsString = result.actions[0].out
+                                    def logsErr = result.actions[0].err
+        							//println logsErr
+        							
+                                    echo "Build status: ${result.out}"
+        					    }
+    						}
+    					}
+                    }
+				}
+            }
+        }
+    }
+    post {
+        success {
+            echo 'Success'
+        }
+        unstable {
+            echo 'Unstable'
+        }
+        failure {
+            echo 'Error'
         }
     }
 }
