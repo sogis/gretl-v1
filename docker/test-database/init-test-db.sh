5993052d2b30e196947b13b725c1e61d6ad55ae0
Christoph Raaflaub <raaflaub@puzzle.ch>
2017-12-28 10:11:14 +0100

    adopt official db init script style

--- docker/test-database/init-test-db.sh
+++ docker/test-database/init-test-db.sh
@@ -1,11 +1,13 @@
-#!/usr/bin/env bash
+#!/bin/bash
+set -e
 
-psql -c "CREATE ROLE ddluser WITH LOGIN PASSWORD 'ddluser';" -U postgres
-psql -c "GRANT CREATE ON DATABASE gretl TO ddluser;" -U postgres
-psql -c "CREATE ROLE dmluser WITH LOGIN PASSWORD 'dmluser';" -U postgres
-psql -c "CREATE ROLE readeruser WITH LOGIN PASSWORD 'readeruser';" -U postgres
-psql -c 'select version();' -d gretl -U postgres
-psql -c 'create extension postgis;' -d gretl -U postgres
-psql -c 'create extension "uuid-ossp";' -d gretl -U postgres
-psql -c 'select postgis_full_version();' -d gretl -U postgres
-
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+    CREATE ROLE ddluser WITH LOGIN PASSWORD 'ddluser';
+    GRANT CREATE ON DATABASE gretl TO ddluser;
+    CREATE ROLE dmluser WITH LOGIN PASSWORD 'dmluser';
+    CREATE ROLE readeruser WITH LOGIN PASSWORD 'readeruser';
+    select version();
+    create extension postgis;
+    create extension "uuid-ossp";
+    select postgis_full_version();
+EOSQL
