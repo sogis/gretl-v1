98f22e568083d62d3701d786c39e16a49e4102e6
Christoph Raaflaub <raaflaub@puzzle.ch>
2018-01-12 08:24:08 +0100

    more gretl test examples

---
+++ docker/test-gretl2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+job_directory=$(pwd)/../inttest/jobs/iliValidator
+task_name=validate
+
+
+# run GRETL job by GRETL runtime
+./start-gretl.sh --docker_image chrira/jobrunner:19 --job_directory $job_directory --task_name $task_name
