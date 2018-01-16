794ce0aad470659b470be5a1ffe0823ee8c078ef
claeis <ce@eisenhutinformatik.ch>
2018-01-16 16:32:32 +0100

    fixed compile error

--- src/main/java/ch/so/agi/gretl/steps/SqlExecutorStep.java
+++ src/main/java/ch/so/agi/gretl/steps/SqlExecutorStep.java
@@ -47,6 +47,10 @@ public class SqlExecutorStep {
      * @throws Exception    if File is missing, no correct extension, no connection to database, could not read file or
      *                      problems while executing sql-queries
      */
+    public void execute(Connector trans, List<File> sqlfiles)
+            throws Exception {
+        execute(trans, sqlfiles,null);
+    }
     public void execute(Connector trans, List<File> sqlfiles,Map<String,String> params)
             throws Exception {
 
