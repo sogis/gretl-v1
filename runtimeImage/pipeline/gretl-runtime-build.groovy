7be617204bc21decb34f9c482ee026d991094e21
Christoph Raaflaub <raaflaub@puzzle.ch>
2018-01-31 16:03:28 +0100

    integrate integration tests to GRETL runtime build

--- runtimeImage/pipeline/gretl-runtime-build.groovy
+++ runtimeImage/pipeline/gretl-runtime-build.groovy
@@ -1,8 +1,6 @@
 /**
  * Builds and tests the GRETL jar and starts an OpenShift Docker build.
- * The Docker image will be pushed to the repository with the build number as tag.
  * Needed parameter:
- * - repository: repository incl. user / organisation to push the Docker image to (text parameter)
  * - openShiftCluster: OpenShift Cluster to be used. (text parameter)
  * - openShiftProject: OpenShift project to be used (text parameter)
  * - ocToolName: Jenkins custom tool name of oc client (text parameter)
@@ -18,7 +16,6 @@ pipeline {
         stage('check parameter') {
             steps {
                 script {
-                    check.mandatoryParameter('repository')
                     check.mandatoryParameter('openShiftCluster')
                     check.mandatoryParameter('openShiftProject')
                     check.mandatoryParameter('ocToolName')
@@ -92,7 +89,78 @@ pipeline {
                 }
             }
         }
-        stage('prepare') {
+        stage('prepare int. test') {
+            steps {
+                // which GRETL jar?
+                sh 'ls -la gretl/build/libs/'
+
+                sh 'rm -rf test-tmp'
+                sh 'mkdir -p test-tmp/inttest'
+
+                // prepare files needed by the build
+                sh 'cp -R gretl/inttest/* test-tmp/inttest/'
+                sh 'ls -la test-tmp/inttest'
+            }
+        }
+        stage('run int. test') {
+            steps {
+                script{
+                    timeout(10) {
+                        def ocDir = tool params.ocToolName
+                        withEnv(["PATH+OC=${ocDir}"]) {
+                            sh "oc version"
+                            openshift.withCluster(params.openShiftCluster, params.openShiftDeployTokenName) {
+                                openshift.withProject(params.openShiftProject) {
+                                    echo "Running in project: ${openshift.project()}"
+
+                                    // find database pod with name: postgresql
+                                    def podSelector = openshift.selector('pods', [name: 'postgres-gis'])
+                                    def dbPod = podSelector.name()
+                                    String shortName = dbPod.indexOf("/") > 0 ? dbPod.substring(dbPod.indexOf("/") + 1): dbPod;
+                                    println "pod: " + shortName
+
+                                    // prepare database
+                                    openshift.raw("cp", "runtimeImage/pipeline/scripts/reset-test-db.sh", "${shortName}:/tmp/")
+                                    echo openshift.exec(shortName, 'bash', '/tmp/reset-test-db.sh').out
+
+                                    openshift.raw("cp", "runtimeImage/pipeline/scripts/init-test-db.sh", "${shortName}:/tmp/")
+                                    echo openshift.exec(shortName, 'bash', '/tmp/init-test-db.sh').out
+
+                                    parallel(
+                                            port_forward: {
+                                                int buildTime = 1
+                                                if (params.buildTime != null) {
+                                                    buildTime = params.buildTime as int
+                                                }
+                                                try {
+                                                    timeout(buildTime) {
+                                                        echo "forward database port"
+                                                        openshift.raw( 'port-forward', shortName, '5432:5432' )
+                                                    }
+                                                } catch (err) {} // catch timeout
+                                                println "port forward done (time: ${buildTime})"
+                                            },
+                                            test: {
+                                                echo "run integration tests ..."
+                                                dir('test-tmp/inttest') {
+                                                    sh './gradlew -version'
+                                                    sh './gradlew clean build testIntegration --refresh-dependencies'
+                                                }
+                                            }
+                                    )
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            post {
+                always {
+                    junit 'test-tmp/inttest/build/test-results/**/*.xml'  // Requires JUnit plugin
+                }
+            }
+        }
+        stage('prepare OCP build') {
             steps {
                 sh 'rm -rf build-tmp'
                 sh 'mkdir build-tmp'
