8bcfbd2f598f2d79a5a0a2a82636a68ddd3928ed
Christoph Raaflaub <raaflaub@puzzle.ch>
2018-02-20 13:19:23 +0100

    add missing import

--- gretl/inttest/src/testSystem/java/ch/so/agi/gretl/jenkins/JenkinsTest.java
+++ gretl/inttest/src/testSystem/java/ch/so/agi/gretl/jenkins/JenkinsTest.java
@@ -8,6 +8,7 @@ import com.offbytwo.jenkins.model.FolderJob;
 import com.offbytwo.jenkins.model.Job;
 import org.junit.Before;
 import org.junit.FixMethodOrder;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
 
@@ -223,7 +224,7 @@ public class JenkinsTest {
         // then
         assertThat(job, not(nullValue()));
     }
-    
+
     @Ignore("until pod is ready")
     @Test
     public void test09shouldBuildOracleLibsPresentJob() throws Exception {
