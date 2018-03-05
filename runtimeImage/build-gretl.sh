f00ce33a573dda11fd7006f7a8b5559a16812e01
uncoyote <jeker.git@outlook.de>
2018-03-05 07:50:06 +0100

    modified .gitignore to exclude all folder starting with __

--- runtimeImage/build-gretl.sh
+++ runtimeImage/build-gretl.sh
@@ -6,21 +6,42 @@ echo "Uses the following container definition:"
 echo "https://github.com/openshift/jenkins/blob/master/slave-maven/Dockerfile"
 echo "======================================================================="
 
-# TODO no version lock
-cp ../gretl/build/libs/gretl-1.0.4-SNAPSHOT.jar gretl
-cp ../gretl/lib/ojdbc7-*.jar gretl
-cp ../dependencies.gradle gretl
+
+
+../gradlew stageJars #copy all jar dependencies to __jars4image
+cp ../gretl/build/libs/gretl-1.0.4-SNAPSHOT.jar gretl/__jars4image
+cp ../gretl/lib/ojdbc7-*.jar gretl/__jars4image
+
+# Set the image labels to the given shell params or sensitive defaults
+
+githash=$1
+if [ "x$githash" = "x" ]; then
+    githash='localbuild'''
+fi
+
+buildnum=$2
+if [ "x$buildnum" = "x" ]; then
+    buildnum=-99
+fi
+
+build_timestamp=$(date '+%Y-%m-%d_%H:%M:%S')
 
 # build infos
-echo "local build" > gretl/build.info
-echo date: `date '+%Y-%m-%d %H:%M:%S'` >> gretl/build.info
+#echo "local build" > gretl/build.info
+#echo date: `date '+%Y-%m-%d %H:%M:%S'` >> gretl/build.info
 
-docker build --no-cache --force-rm -t gretl-runtime -f gretl/Dockerfile gretl
+docker build \
+    --no-cache --force-rm -t gretl-runtime \
+    --label gretl.created=$build_timestamp --label gretl.git_commit=$githash --label gretl.travis_build=$buildnum \
+    -f gretl/Dockerfile gretl
 
-rm gretl/gretl*.jar
-rm gretl/ojdbc7-*.jar
-rm gretl/dependencies.gradle
+rm gretl/__jars4image/*
 rm gretl/build.info
 
 # look into the container:
 # docker run -it --entrypoint=/bin/sh gretl-runtime
+
+#todo
+#image build auf travis
+#überflüssiges build.gradle weg
+
