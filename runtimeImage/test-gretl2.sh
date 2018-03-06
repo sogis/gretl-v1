b4cffa8af4b0b197d1a05ea16f2af697471ef1cc
uncoyote <jeker.git@outlook.de>
2018-03-06 07:35:37 +0100

    added dockerhub push to travis pipeline

--- runtimeImage/test-gretl2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-job_directory=$(pwd)/../gretl/inttest/jobs/iliValidator
-task_name=validate
-
-
-# run GRETL job by GRETL runtime
-./start-gretl.sh --job_directory $job_directory --task_name $task_name
