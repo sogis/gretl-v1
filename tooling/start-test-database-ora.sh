6dcf4f11b2e10dcf79a11d1bf4f211a8c0a2ac03
uncoyote <jeker.git@outlook.de>
2018-03-07 13:57:16 +0100

    travis: starting ora image in backgroud

--- tooling/start-test-database-ora.sh
+++ tooling/start-test-database-ora.sh
@@ -6,10 +6,27 @@ echo "Uses the following Docker image:"
 echo "https://hub.docker.com/r/pengbai/docker-oracle-12c-r1/"
 echo "=========================================================="
 
-docker run -it \
-    --name test-oracle \
-    -p 1521:1521 \
-    pengbai/docker-oracle-12c-r1
+
+if [ "x$1" = "x" ]; then
+
+    docker run -it \
+        --name test-oracle \
+        -p 1521:1521 \
+        pengbai/docker-oracle-12c-r1
+
+else #run image in background
+
+    echo 'Pulling image in foreground, running in background. Oracle listening on localhost:1521'
+
+    docker pull pengbai/docker-oracle-12c-r1
+
+    docker run -d \
+        --name test-oracle \
+        -p 1521:1521 \
+        pengbai/docker-oracle-12c-r1
+fi
+
+
 
 # hostname: localhost
 # port: 1521
